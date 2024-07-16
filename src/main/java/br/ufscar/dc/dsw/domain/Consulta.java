5c24a62bc16fbf6a0b5ff91869181f6ff2ba99cb
lucasgoncalves-code <lucas.goncalves@vonbraunlabs.com.br>
2024-07-16 10:38:49 -0300

    Criação dos domains

---
+++ src/main/java/br/ufscar/dc/dsw/domain/Consulta.java
@@ -0,0 +1,43 @@
+package br.ufscar.dc.dsw.domain;
+
+import java.time.LocalDateTime;
+
+public class Consulta {
+    private int id;
+    private String pacienteCpf;
+    private String medicoCrm;
+    private LocalDateTime dataHora;
+
+    
+    public Consulta(int id, String pacienteCpf, String medicoCrm, LocalDateTime dataHora) {
+        this.id = id;
+        this.pacienteCpf = pacienteCpf;
+        this.medicoCrm = medicoCrm;
+        this.dataHora = dataHora;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPacienteCpf() {
+        return pacienteCpf;
+    }
+    public void setPacienteCpf(String pacienteCpf) {
+        this.pacienteCpf = pacienteCpf;
+    }
+    public String getMedicoCrm() {
+        return medicoCrm;
+    }
+    public void setMedicoCrm(String medicoCrm) {
+        this.medicoCrm = medicoCrm;
+    }
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+}
---
+++ src/main/java/br/ufscar/dc/dsw/domain/Paciente.java
@@ -0,0 +1,80 @@
+package br.ufscar.dc.dsw.domain;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    private String email;
+    private String senha;
+    private String cpf;
+    private String nome;
+    private String telefone;
+    private String sexo;
+    private LocalDate dataNascimento;
+    
+    public Paciente(String email, String senha, String cpf, String nome, String telefone, String sexo,
+            LocalDate dataNascimento) {
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
