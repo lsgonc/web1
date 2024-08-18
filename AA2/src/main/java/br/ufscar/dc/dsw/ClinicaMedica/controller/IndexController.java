071b22c1ac7733ea98e765733fdb0305fbc6dac3
Enzo Murayama <enzomurayama@gmail.com>
2024-08-18 09:24:30 -0300

    Criando index e arrumando arquivos de linguagem

---
+++ AA2/src/main/java/br/ufscar/dc/dsw/ClinicaMedica/controller/IndexController.java
@@ -0,0 +1,23 @@
+package br.ufscar.dc.dsw.ClinicaMedica.controller;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model, Locale locale) {
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.FULL, locale);
+    	
+    	Calendar cal = Calendar.getInstance();
+        model.addAttribute("dateString", dateFormat.format(cal.getTime()));
+        model.addAttribute("date", cal.getTime());
+    
+        return "index";
+    }
+}
