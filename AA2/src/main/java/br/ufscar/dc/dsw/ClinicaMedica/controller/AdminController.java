c90904de999816646b694b1842478caba5e4299f
lucasgoncalves-vbl <lucas.goncalves@vonbraunlabs.com.br>
2024-08-18 23:53:28 -0300

    Admin controller e view

--- AA2/src/main/java/br/ufscar/dc/dsw/ClinicaMedica/controller/AdminController.java
+++ AA2/src/main/java/br/ufscar/dc/dsw/ClinicaMedica/controller/AdminController.java
@@ -2,8 +2,13 @@ package br.ufscar.dc.dsw.ClinicaMedica.controller;
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
+import br.ufscar.dc.dsw.ClinicaMedica.dao.IMedicoDAO;
+import br.ufscar.dc.dsw.ClinicaMedica.dao.IPacienteDAO;
+import br.ufscar.dc.dsw.ClinicaMedica.domain.Medico;
+import br.ufscar.dc.dsw.ClinicaMedica.domain.Paciente;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +16,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
+    private final IMedicoDAO medicoDAO;
+    private final IPacienteDAO pacienteDAO;
+
+    public AdminController(IMedicoDAO medicoDAO, IPacienteDAO pacienteDAO) {
+        this.medicoDAO = medicoDAO;
+        this.pacienteDAO = pacienteDAO;
+    }
+
     @GetMapping("/admin")
     public String index(Model model, Locale locale) {
+        List<Medico> listaMedicos = medicoDAO.findAll();
+        List<Paciente> listaPacientes = pacienteDAO.findAll();
 
+        model.addAttribute("listaMedicos", listaMedicos);
+        model.addAttribute("listaPacientes", listaPacientes);
 
         return "admin/index";
     }
