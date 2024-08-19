2ca19b3057d608e5e0f27477f2c9275ee9336816
Antonio Maldonado <maldonadoagc@gmail.com>
2024-08-19 11:40:28 -0300

    fix lista medicos

--- AA2/src/main/java/br/ufscar/dc/dsw/ClinicaMedica/controller/MedicoController.java
+++ AA2/src/main/java/br/ufscar/dc/dsw/ClinicaMedica/controller/MedicoController.java
@@ -2,27 +2,27 @@ package br.ufscar.dc.dsw.ClinicaMedica.controller;
 
 import br.ufscar.dc.dsw.ClinicaMedica.dao.IConsultaDAO;
 import br.ufscar.dc.dsw.ClinicaMedica.dao.IMedicoDAO;
-import br.ufscar.dc.dsw.ClinicaMedica.dao.IUsuarioDAO;
+import br.ufscar.dc.dsw.ClinicaMedica.domain.Medico;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import br.ufscar.dc.dsw.ClinicaMedica.dao.IPacienteDAO;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class MedicoController {
 
+    private final IConsultaDAO consultaDAO;
+    private final IMedicoDAO medicoDAO;
+
     @Autowired
-
-    private IConsultaDAO consultaDAO;
-    private IMedicoDAO medicoDAO;
-
     public MedicoController(IConsultaDAO consultaDAO, IMedicoDAO medicoDAO) {
-
         this.consultaDAO = consultaDAO;
         this.medicoDAO = medicoDAO;
     }
@@ -33,16 +33,24 @@ public class MedicoController {
 
         try {
             consultaDAO.deleteByMedicoCRM(CRM);
-
-
             medicoDAO.deleteByCRM(CRM);
-
-
-            redirectAttributes.addFlashAttribute("message", "removeu.");
+            redirectAttributes.addFlashAttribute("message", "Médico removido com sucesso.");
         } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("error", "erro.");
+            redirectAttributes.addFlashAttribute("error", "Erro ao remover médico.");
         }
 
         return new RedirectView("/admin");
+    }
+
+    @GetMapping("/medico")
+    public String listarMedicos(Model model) { 
+        try {
+            List<Medico> medicos = medicoDAO.findAll(); 
+            model.addAttribute("listaMedicos", medicos);
+        } catch (Exception e) {
+            model.addAttribute("error", "Erro ao listar médicos.");
+        }
+
+        return "medicos/index"; 
     }
 }
